76f2f3f6f8c6ec8f246acdd23cea47272a70d064
kejiacheng <kejc@shinemo.com>
2017-09-15 17:50:42 +0800

    0915

--- select/select.js
+++ select/select.js
@@ -1,3 +1,17 @@
-/**
- * Created by ShineMo-177 on 2017/9/15.
- */
+function keSelect (opts) {
+    if (opts.target instanceof jQuery) {
+
+    }else{
+        throw('请添加jQuery对象！')
+    }
+
+    this.target = opts.target
+    this.data = opts.data || []
+    this.cb = opts.cb || function () {}
+
+
+}
+
+keSelect.prototype.reload = function () {
+    console.log(123456)
+}
