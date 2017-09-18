eb9f7b85b345de91d15a0dcaa7bda1291a432c7c
kejiacheng <kejc@shinemo.com>
2017-09-18 17:23:07 +0800

    0918

--- select/select.js
+++ select/select.js
@@ -1,9 +1,31 @@
+$(document).mousedown(function (e) {
+    var
+        target =    $(e.target).hasClass('ke-selected-box')
+                        ? $(e.target)
+                        : $(e.target).parent().hasClass('ke-selected-box')
+                            ?   $(e.target).parent()
+                            :   null
+
+    if (target) {
+        $('.ke-selected-box').not(target).find('.ke-select-down-arrow').removeClass('ke-select-down-arrow-active')
+        $('.ke-select-item-wrapper').not(target.next()).slideUp('fast')
+        return false
+    }else{
+        if (!$(e.target).hasClass('ke-select-item')) {
+            $('.ke-select-down-arrow').removeClass('ke-select-down-arrow-active')
+            $('.ke-select-item-wrapper').slideUp('fast')
+        }
+    }
+})
+
 function keSelect (opts) {
     if (opts.target instanceof jQuery) {
 
     }else{
         throw('请添加jQuery对象！')
     }
+    var
+        that = this
 
     this.target = opts.target
     this.data = opts.data || []
@@ -11,43 +33,72 @@ function keSelect (opts) {
     this.wrapperClick = opts.wrapperClick || function () {}
     this.cb = opts.cb || function () {}
 
-    function renderSelect(target, data, defaultSelected, that) {
+    function renderBaseHtml() {
+        var
+            str = ''
+
+        str +=  '<div class="ke-selected-box">'+
+                    '<span class="ke-selected-text"></span>'+
+                    '<span class="ke-select-down-arrow"></span>'+
+                '</div>'+
+                '<div class="ke-select-item-wrapper">'+
+                '</div>'
+
+        that.target.html(str)
+
+        that.target.find('.ke-selected-box').unbind('click').click(function (e) {
+            that.wrapperClick()
+            $(this).next().slideDown('fast')
+
+            $(this).find('.ke-select-down-arrow').addClass('ke-select-down-arrow-active')
+            e.stopPropagation()
+        })
+    }
+
+    this.renderOption = function(target, data, defaultSelected, cb) {
         var
             str = '',
             selectedItem = '',
-            dropDownBox = '<div class="ke-item-wrapper">'
+            dropDownOptions = ''
 
         for (var i = 0; i < data.length; i++) {
             if (data[i].value === defaultSelected) {
-                selectedItem =  '<div class="ke-selected-box" data-value="'+ data[i].value +'">'+
-                                    '<span class="ke-selected-text">'+ data[i].text +'</span>'+
-                                    '<span class="down-arrow"></span>'+
-                                '</div>'
+                target.find('.ke-selected-box').attr('data-value', data[i].value)
+                target.find('.ke-selected-text').text(data[i].text)
 
-                dropDownBox +=  '<div class="ke-item ke-selected-item" data-value="'+ data[i].value +'">'+
+                dropDownOptions +=  '<div class="ke-select-item ke-selected-item" data-value="'+ data[i].value +'">'+
                                     data[i].text+
                                 '</div>'
             }else{
-                dropDownBox +=  '<div class="ke-item" data-value="'+ data[i].value +'">'+
+                dropDownOptions +=  '<div class="ke-select-item" data-value="'+ data[i].value +'">'+
                                     data[i].text+
                                 '</div>'
             }
         }
 
-        dropDownBox += '</div>'
-        str = selectedItem + dropDownBox
+        str = dropDownOptions
 
-        target.html(str)
+        target.find('.ke-select-item-wrapper').html(str)
 
-        $('.ke-selected-box').click(function () {
+        target.find('.ke-select-item').unbind('click').click(function (e) {
+            var
+                value = $(this).data('value'),
+                text = $(this).text()
+            target.find('.ke-selected-box').attr('data-value', value)
+                .find('.ke-select-down-arrow').removeClass('ke-select-down-arrow-active')
+            target.find('.ke-selected-text').text(text)
+            target.find('.ke-selected-item').removeClass('ke-selected-item')
+            $(this).addClass('ke-selected-item')
 
+            $('.ke-select-item-wrapper').slideUp('fast')
+            cb(value, text, $(this).index())
         })
     }
 
-    renderSelect(this.target, this.data, this.defaultSelected, this)
+    renderBaseHtml()
+    this.renderOption(this.target, this.data, this.defaultSelected, this.cb)
 }
 
-keSelect.prototype.reload = function () {
-    console.log(this.defaultSelected)
-    console.log(123456)
+keSelect.prototype.reload = function (opts) {
+    this.renderOption(opts.target, opts.data, opts.defaultSelected, opts.cb)
 }
